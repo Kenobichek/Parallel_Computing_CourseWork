1a702067d66414bef49a47c5fd5bdfdf41ed80f3
Olesia Lesechko <lesechklesia@gmail.com>
2023-12-10 22:55:23 +0200

    Implemented ThreadPool Constructor, Destructor, Routine, AddTask, and Terminate functions

---
+++ src/ThreadPool.cc
@@ -0,0 +1,88 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(const int thread_count) {
+	write_lock _(rw_lock);
+
+	if (initialized || terminated) {
+		return;
+	}
+
+	threads.reserve(thread_count);
+
+	for (int i = 0; i < thread_count; i++) {
+		threads.emplace_back(&ThreadPool::Routine, this);
+	}
+
+	initialized = !threads.empty();
+}
+
+ThreadPool::~ThreadPool() {
+	Terminate();
+}
+
+void ThreadPool::Routine()
+{
+	while (true)
+	{
+		std::unique_lock lock(rw_lock);
+		task_waiter.wait(rw_lock, [this] { 
+			return terminated || !tasks_queue.empty(); 
+		});
+
+		if (terminated && tasks_queue.empty()) {
+			return;
+		}
+
+		auto task = tasks_queue.front();
+		tasks_queue.pop();
+
+		lock.unlock();
+
+		task();
+	}
+}
+
+void ThreadPool::AddTask(std::function<void()> fun) {
+	read_lock _(rw_lock);
+	if (!WorkingUnsafe()) {
+		return;
+	}
+
+	if (tasks_queue.emplace(fun)) {
+		task_waiter.notify_one();
+	}
+}
+
+void ThreadPool::Terminate(const bool should_finish_tasks) {
+	{
+		write_lock _(rw_lock);
+
+		if (!should_finish_tasks) {
+			threads.clear();
+		}
+		if (WorkingUnsafe()) {
+			terminated = true;
+		}
+		else {
+			threads.clear();
+			terminated = false;
+			initialized = false;
+			return;
+		}
+	}
+
+	task_waiter.notify_all();
+
+	for (std::thread& thread : threads) {
+		thread.join();
+	}
+
+	threads.clear();
+	terminated = false;
+	initialized = false;
+}
+
+bool ThreadPool::WorkingUnsafe() const
+{
+	return initialized && !terminated;
+}
---
+++ src/ThreadPool.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <vector>
+#include <thread>
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <shared_mutex>
+#include <queue>
+
+using read_write_lock = std::shared_mutex;
+using read_lock = std::shared_lock<read_write_lock>;
+using write_lock = std::unique_lock<read_write_lock>;
+
+class ThreadPool
+{
+	public:
+		ThreadPool(const int thread_count = 4);
+		~ThreadPool();
+		void AddTask(std::function<void()>);
+		void Terminate(const bool should_finish_tasks = true);
+		void Routine();
+		bool WorkingUnsafe() const;
+
+	private:
+		mutable read_write_lock rw_lock;
+		mutable std::condition_variable_any task_waiter;
+
+		std::vector<std::thread> threads;
+		std::queue<std::function<void()>> tasks_queue;
+
+		bool initialized = false;
+		bool terminated = false;
+};
