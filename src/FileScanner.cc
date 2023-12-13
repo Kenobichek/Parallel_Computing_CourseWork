cd9ecc6b064857b1941e2f708d7cc160684cfd65
Olesia Lesechko <lesechklesia@gmail.com>
2023-12-13 20:08:28 +0200

    Refactor ProcessFolder to Synchronize Task Completion

--- src/FileScanner.cc
+++ src/FileScanner.cc
@@ -5,17 +5,33 @@
 #include "FileScanner.h"
 
 void FileScanner::ProcessFolder() {
+	int tasks_count = 0;
 	for (const auto& entry : std::filesystem::directory_iterator(folder_path)) {
 		if (entry.is_regular_file()) {
-			thread_pool.AddTask([this, entry]() {
-				ProcessFile(entry.path().string());
+			thread_pool.AddTask([this, entry, &tasks_count]() {
+				ProcessFile(entry.path());
+				{
+					write_lock _(rw_lock);
+					--tasks_count;
+				}
+				termination_cv.notify_one();
 			});
+			{
+				write_lock _(rw_lock);
+				++tasks_count;
+			}
 		}
 	}
+
+	write_lock lock(rw_lock);
+
+	termination_cv.wait(rw_lock, [&tasks_count] {
+		return !tasks_count; 
+	});
 }
 
 
-void FileScanner::ProcessFile(const std::string& filepath) {
+void FileScanner::ProcessFile(const std::filesystem::path& filepath) {
 	std::ifstream file(filepath);
 	if (!file.is_open()) {
 		std::cout << "Failed to open file: " << filepath << std::endl;
@@ -24,8 +40,7 @@ void FileScanner::ProcessFile(const std::string& filepath) {
 
 	std::string word;
 	while (file >> word) {
-		std::cout << word << std::endl;
-		// inverted_index.AddWord(word, filepath);
+		inverted_index.AddWord(word, filepath);
 	}
 
 	file.close();
--- src/FileScanner.h
+++ src/FileScanner.h
@@ -3,15 +3,22 @@
 #include "InvertedIndex.h"
 #include "ThreadPool.h"
 
+using read_write_lock = std::shared_mutex;
+using read_lock = std::shared_lock<read_write_lock>;
+using write_lock = std::unique_lock<read_write_lock>;
+
 class FileScanner {
 	public:
 		FileScanner(const std::string& folder_path, InvertedIndex& inverted_index)
         : folder_path(folder_path), inverted_index(inverted_index) {}
 
 		void ProcessFolder();
-		void ProcessFile(const std::string& filepath);
+		void ProcessFile(const std::filesystem::path& filepath);
 
 	private:
+		mutable read_write_lock rw_lock;
+		mutable std::condition_variable_any termination_cv;
+
 		std::string folder_path;
 		InvertedIndex& inverted_index;
 		ThreadPool thread_pool;
