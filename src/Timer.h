cdf865a6cb235d3b422c1df15d0ae62f9d5bd2bb
Olesia Lesechko <lesechklesia@gmail.com>
2023-12-28 15:49:40 +0200

    Added tabs to enhance user interface

--- src/Timer.h
+++ src/Timer.h
@@ -4,20 +4,20 @@
 
 class Timer
 {
-public:
-	using timePoint = std::chrono::high_resolution_clock::time_point;
-	using second_t = std::chrono::duration<double, std::ratio<1>>;
+	public:
+		using timePoint = std::chrono::high_resolution_clock::time_point;
+		using second_t = std::chrono::duration<double, std::ratio<1>>;
 
-	static std::shared_ptr<Timer> GetInstance();
-	
-	void ResetTime();
-	double ElapsedTime() const;
+		static std::shared_ptr<Timer> GetInstance();
+		
+		void ResetTime();
+		double ElapsedTime() const;
 
-	Timer(Timer const&) = delete;
-	Timer& operator=(Timer const&) = delete;
+		Timer(Timer const&) = delete;
+		Timer& operator=(Timer const&) = delete;
 
-private:
-	Timer() = default;
-	timePoint startTime;
-	int rejectedTasksCount = 0;
+	private:
+		Timer() = default;
+		timePoint startTime;
+		int rejectedTasksCount = 0;
 };
