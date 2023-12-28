111133e0698a8849e7f55cae3374a02707696dee
Olesia Lesechko <lesechklesia@gmail.com>
2023-12-28 15:48:38 +0200

    Displayed file paths for client-specified word

--- src/Server.cc
+++ src/Server.cc
@@ -1,10 +1,12 @@
 #include "Server.h"
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
 void WebSocketServer::HandleClient() {
 	acceptor.async_accept(socket, [this](const beast::error_code& ec) {
 		if (!ec) {
 			std::cout << "Client connected!" << std::endl;
-			std::make_shared<WebSocketSession>(std::move(socket))->Start();
+			std::make_shared<WebSocketSession>(std::move(socket), inverted_index)->Start();
 		}
 		HandleClient();
 	});
@@ -24,20 +26,45 @@ void WebSocketSession::Read() {
 			std::string received_data(beast::buffers_to_string(self->buffer.data()));
 			std::cout << "Received from client: " << received_data << std::endl;
 
-			self->Write();
+			self->buffer.consume(self->buffer.size());
+			self->Write(received_data);
+			self->Read();
 		} 
 		else {
+			if (ec == beast::websocket::error::closed) {
+				std::cout << "WebSocket connection closed by the client." << std::endl;
+			}
+			else {
 				std::cerr << "Error reading WebSocket frame: " << ec.message() << std::endl;
+			}
 		}
 	});
 }
 
-void WebSocketSession::Write() {
-	std::string message = "Hello Client!!!";
+void WebSocketSession::Write(const std::string& word) {
+	boost::property_tree::ptree pt;
+	Files files = inverted_index.GetFiles(word);
+	boost::property_tree::ptree filesNode;
+
+	for (const auto& filePath : files) {
+		boost::property_tree::ptree fileNode;
+		fileNode.put_value(filePath.string());
+		filesNode.push_back(std::make_pair("", fileNode));
+	}
+	
+	if (!filesNode.empty()) {
+		pt.put_child("filePaths", filesNode);
+	}
+
+	std::ostringstream oss;
+	boost::property_tree::write_json(oss, pt);
+
 	ws.text(true);
-	ws.async_write(asio::buffer(message), [self = shared_from_this()](const beast::error_code& ec, std::size_t) {
+	ws.async_write(asio::buffer(oss.str()), 
+		[self = shared_from_this()](const beast::error_code& ec, std::size_t) {
 			if (ec) {
-					std::cerr << "Error writing WebSocket frame: " << ec.message() << std::endl;
+				std::cerr << "Error writing WebSocket frame: " << ec.message() << std::endl;
 			}
-	});
+		}
+	);
 }
--- src/Server.h
+++ src/Server.h
@@ -1,6 +1,9 @@
+#pragma once
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/beast.hpp>
+#include "InvertedIndex.h"
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -9,29 +12,33 @@ namespace websocket = boost::beast::websocket;
 
 class WebSocketSession : public std::enable_shared_from_this<WebSocketSession> {
 	public:
-		WebSocketSession(asio::ip::tcp::socket&& socket)
-			: ws(std::move(socket)) {}
+		WebSocketSession(asio::ip::tcp::socket&& socket, InvertedIndex& index)
+			: ws(std::move(socket)), inverted_index(index) {}
 
 		void Start();
 		void Read();
-		void Write();
+		void Write(const std::string& word);
 
 	private:
 		websocket::stream<asio::ip::tcp::socket> ws;
 		beast::flat_buffer buffer;
+
+		InvertedIndex& inverted_index;
 };
 
 class WebSocketServer {
 	public:
-		WebSocketServer(asio::io_context& io_context, short port)
+		WebSocketServer(asio::io_context& io_context, short port, InvertedIndex& inverted_index)
 			: acceptor(io_context, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port)),
-			socket(io_context) {
+			socket(io_context), inverted_index(inverted_index) {
 			HandleClient();
 		}
 
 	private:
 		asio::ip::tcp::acceptor acceptor;
 		asio::ip::tcp::socket socket;
+
+		InvertedIndex& inverted_index;
 
 		void HandleClient();
 };
